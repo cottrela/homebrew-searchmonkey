2f37300aaa1e8145ab02a1f379fd6b595b08afc7
Adam Cottrell <33880683+cottrela@users.noreply.github.com>
2026-05-01 22:41:36 +0100

    Adding brew formula

---
+++ Formula/searchmonkey.rb
@@ -0,0 +1,15 @@
+class Searchmonkey < Formula
+  desc "Real-time file search tool with regex support"
+  homepage "https://searchmonkey.dev"
+  url "https://github.com/cottrela/searchmonkey-III/releases/download/v0.1.3/searchmonkey_universal.app.tar.gz"
+  sha256 "0e823fe7d9982432a612bd873aa78b81fc59e212416f3600d69a96702eddd8a7"
+  license "MIT"
+
+  def install
+    bin.install "searchmonkey"
+  end
+
+  test do
+    system "#{bin}/searchmonkey", "--version"
+  end
+end
