62fe594ba7ddb1a8966c79305ba31ec26aa80a10
Adam Cottrell <33880683+cottrela@users.noreply.github.com>
2026-05-01 23:03:58 +0100

    Fixing path typo

--- Casks/searchmonkey.rb
+++ Casks/searchmonkey.rb
@@ -2,7 +2,7 @@ cask "searchmonkey" do
   version "0.1.3"
   sha256 "0e823fe7d9982432a612bd873aa78b81fc59e212416f3600d69a96702eddd8a7"
 
-  url "https://github.com/cottrela/searchmonkey-III/releases/download/v#{version}/Searchmonkey_#{version}_universal.app.tar.gz"
+  url "https://github.com/cottrela/searchmonkey-III/releases/download/v#{version}/searchmonkey_universal.app.tar.gz"
   name "Searchmonkey III"
   desc "Real-time file search tool with regex support"
   homepage "https://searchmonkey.dev"
