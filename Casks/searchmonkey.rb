6741a690140b4537910c99ac58ad0df5fdd3766b
Adam Cottrell <33880683+cottrela@users.noreply.github.com>
2026-05-24 13:36:26 +0100

    Releasing v0.3.0

--- Casks/searchmonkey.rb
+++ Casks/searchmonkey.rb
@@ -1,8 +1,8 @@
 cask "searchmonkey" do
-  version "0.2.4"
-  sha256 "c77f39ce70e8bd977b1560c699c5af225c1d025881c90eea7f9f5098a18a638c"
+  version "0.3.0"
+  sha256 "4f78341335d878d6ae3eb261007e76ea82626a133518782d68dcf808b448fab6"
 
-  url "https://github.com/cottrela/searchmonkey-III/releases/download/v#{version}/searchmonkey_universal.app.tar.gz"
+  url "https://github.com/cottrela/searchmonkey-III/releases/download/v#{version}/Searchmonkey_universal.app.tar.gz"
   name "Searchmonkey III"
   desc "Real-time file search tool with regex support"
   homepage "https://searchmonkey.dev"
